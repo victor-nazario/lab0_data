fca891099b4a1993f3f9356398af6d51b7378e83
Victor Nazario <victor@Victors-MacBook-Pro.local>
2018-02-24 21:59:56 -0400

    Area circle

---
+++ AreaCircle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AreaCircle {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println(areaCalculator(in.nextDouble()));
+	}
+
+	
+	public static double areaCalculator(double radius) {
+			
+		return Math.PI * Math.pow(radius, 2);
+	}
+}
